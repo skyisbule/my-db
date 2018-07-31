f05a6f2f54370c8954de539fb6969a03831bd157
skyisbule <heqiut@qq.com>
2018-07-31 10:07:25 +0800

    定义serverSocketThread。

---
+++ src/main/java/skyisbule/github/com/thread/ServerSocketThreadImp.java
@@ -0,0 +1,16 @@
+package skyisbule.github.com.thread;
+
+import skyisbule.github.com.task.IoTask;
+
+//定义连接客户端的监听线程，用于解析用于请求，解析sql，构造ioTask并递交给mainThread
+public class ServerSocketThreadImp extends Thread implements ServerSocketThread {
+
+    public void run(){
+
+    }
+
+    public void doIoCallBack(IoTask ioTask) {
+
+    }
+
+}
